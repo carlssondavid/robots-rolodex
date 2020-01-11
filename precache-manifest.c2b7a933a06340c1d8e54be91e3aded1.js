f4c8cd67a4d70f2909ccbac5b93e5a08755e1d59
David Carlsson <carlssondavid@outlook.com>
2020-01-11 11:18:24 +0100

    Updates

---
+++ precache-manifest.c2b7a933a06340c1d8e54be91e3aded1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d35de16544579216004045dff1771a4",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "03f0b3f8c7882285d3c6",
+    "url": "/robots-rolodex/static/css/main.7faf4612.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/robots-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robots-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03f0b3f8c7882285d3c6",
+    "url": "/robots-rolodex/static/js/main.70b945cc.chunk.js"
+  },
+  {
+    "revision": "dff7459d7bd24c5de642",
+    "url": "/robots-rolodex/static/js/runtime-main.131b7ecb.js"
+  }
+]);
